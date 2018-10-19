74560f9860b59f0d292c6a9e0e307ae816f52541
Kevin Davis <kjydavis3@gmail.com>
2018-10-19 11:16:21 -0400

    Update xrp paychan, use latest lightning plugin

--- plugins/strata4-xrp.config.js
+++ plugins/strata4-xrp.config.js
@@ -1,6 +1,8 @@
 const BigNumber = require('bignumber.js')
-const maximum = new BigNumber(10).shiftedBy(9).toString()
-const settleThreshold = new BigNumber(maximum).dividedBy(2).toString()
+// const maximum = new BigNumber(10).shiftedBy(9).toString()
+const maximum = new BigNumber(9).shiftedBy(9).toString()
+// const settleThreshold = new BigNumber(maximum).dividedBy(2).toString()
+const settleThreshold = new BigNumber(-1).shiftedBy(6).toString()
 const maxPacketAmount = new BigNumber('0.1').shiftedBy(9).toString()
 
 module.exports = {
@@ -15,13 +17,13 @@ module.exports = {
     settleTo: '0',
   },
   options: {
-    peerAddress: 'rNJ25qzwhqkrQY8WmhwtoGotPDgtqGmqPL',
+    // peerAddress: 'rfES1negveYfX9TpV8tapBvye8a52pXBHG',
     assetScale: 9,
     listener: {
       port: 7444,
-      secret: process.env.XRP_STRATA_3_SECRET,
+      secret: process.env.XRP_STRATA_3_SECRET
     },
-    xrpServer: 'wss://s2.ripple.com',
+    xrpServer: 'wss://s.altnet.rippletest.net:51233',
     address: process.env.XRP_ADDRESS,
     secret: process.env.XRP_SECRET,
   }
