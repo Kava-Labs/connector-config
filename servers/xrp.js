8b6806b25596602945a94b88cd1923c4d21e0d31
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-12-10 12:19:05 -0500

    feat(xrp): $10 outgoing channel amount

---
+++ servers/xrp.js
@@ -0,0 +1,23 @@
+const { convert, usd, xrpBase, drop } = require('@kava-labs/crypto-rate-utils')
+
+module.exports = rateApi => {
+  const outgoingChannelAmount = convert(usd(10), drop(), rateApi).toNumber()
+  const maxPacketAmount = convert(usd(0.1), xrpBase(), rateApi)
+
+  return {
+    relation: 'child',
+    plugin: 'ilp-plugin-xrp-asym-server',
+    assetCode: 'XRP',
+    assetScale: 9,
+    options: {
+      assetScale: 9,
+      port: 7443,
+      xrpServer: process.env.XRP_SERVER,
+      address: process.env.XRP_ADDRESS,
+      secret: process.env.XRP_SECRET,
+      // Max credit defaults to 0, so it's unnecesssary
+      maxPacketAmount /* In plugin (and not connector middleware) so F08s occur *before* T04s */,
+      outgoingChannelAmount
+    }
+  }
+}
