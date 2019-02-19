2e69f819fde4c358bb09590a4baf652a538ea0c1
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-02-19 16:45:12 -0500

    fix: revert to xrp-asym-server, remove peer

--- run-connector.js
+++ run-connector.js
@@ -1,19 +1,10 @@
 const { createApp } = require('@kava-labs/ilp-connector')
-const {
-  convert,
-  usd,
-  xrp,
-  xrpBase,
-  connectCoinCap
-} = require('@kava-labs/crypto-rate-utils')
+const { connectCoinCap } = require('@kava-labs/crypto-rate-utils')
 const { parse, resolve } = require('path')
 const chokidar = require('chokidar')
 
 async function run() {
   const rateApi = await connectCoinCap()
-
-  const outgoingChannelAmount = convert(usd(10), xrpBase(), rateApi).toString()
-  const maxPacketAmount = convert(usd(0.2), xrpBase(), rateApi).toString()
 
   const config = {
     env: process.env.CONNECTOR_ENV,
@@ -32,33 +23,6 @@ async function run() {
     accountProviders: {
       servers: {
         type: 'plugin'
-      },
-      xrp: {
-        type: 'btp-server',
-        options: {
-          listener: {
-            port: 7443
-          },
-          defaultAccountInfo: {
-            plugin: 'ilp-plugin-xrp-paychan',
-            relation: 'child',
-            assetCode: 'XRP',
-            assetScale: 9,
-            // Options passed into the plugin itself
-            options: {
-              assetScale: 9,
-              xrpServer: process.env.XRP_SERVER,
-              secret: process.env.XRP_SECRET,
-              channelAmount: outgoingChannelAmount
-            },
-            balance: {
-              maximum: '0',
-              settleTo: '0',
-              settleThreshold: '0'
-            },
-            maxPacketAmount
-          }
-        }
       }
     },
     accounts: {}
---
+++ servers/xrp.js
@@ -0,0 +1,27 @@
+const { convert, usd, xrpBase, drop } = require('@kava-labs/crypto-rate-utils')
+
+module.exports = rateApi => {
+  const outgoingChannelAmount = convert(usd(10), drop(), rateApi).toNumber()
+  const minIncomingChannelAmount = convert(usd(0.5), drop(), rateApi).toNumber()
+  const maxPacketAmount = convert(usd(0.2), xrpBase(), rateApi)
+
+  return {
+    relation: 'child',
+    plugin: 'ilp-plugin-xrp-asym-server',
+    assetCode: 'XRP',
+    assetScale: 9,
+    options: {
+      assetScale: 9,
+      port: 7443,
+      xrpServer: process.env.XRP_SERVER,
+      address: process.env.XRP_ADDRESS,
+      secret: process.env.XRP_SECRET,
+      // Max credit defaults to 0, so it's unnecesssary
+      // Use plugin maxPacketAmount (and not connector middleware) so F08s occur *before* T04s
+      maxPacketAmount,
+      // Very asymmetric... you fund a channel for $0.50 in XRP, we'll open one to you for $10!
+      outgoingChannelAmount,
+      minIncomingChannelAmount
+    }
+  }
+}
