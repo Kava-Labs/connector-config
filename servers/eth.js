6482189008dc2134c2bee80ffac637fc8ec7de6c
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-02-26 17:08:57 -0500

    fix(eth): remove deprecated config options

--- servers/eth.js
+++ servers/eth.js
@@ -21,18 +21,12 @@ module.exports = rateBackend => {
     assetScale: 9,
     options: {
       role: 'server',
+      port: 7442,
       ethereumPrivateKey: process.env.ETHEREUM_PRIVATE_KEY,
       ethereumProvider: process.env.ETHEREUM_PROVIDER,
       getGasPrice: process.env.CONNECTOR_ENV === 'production' && getGasPrice,
-      port: 7442,
-      incomingChannelFee: 0,
       outgoingChannelAmount,
       minIncomingChannelAmount,
-      balance: {
-        maximum: '0',
-        settleTo: '0',
-        settleThreshold: '0' // Don't pass 0 as number: falsy value won't get set (bug in plugin)
-      },
       maxPacketAmount // In plugin (and not connector middleware) so F08s occur *before* T04s
     }
   }
