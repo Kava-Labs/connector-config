947a78d5584af5685c704eec475e68b8967a12b1
Robert Laverty <roblav96@users.noreply.github.com>
2019-02-27 13:57:04 -0500

    Scrap dynamic inspect ports
    Not needed since `money-d` is no longer used

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,4 @@
-const os = require('os')
 const path = require('path')
-const execa = require('execa')
 require('envkey')
 
 const debugEnv = {
@@ -24,34 +22,7 @@ const ecosystem = {
       },
       log: '~/.pm2/logs/ilp-connector-combined.log'
     }
-    // {
-    //   cwd: __dirname,
-    //   name: 'moneyd-gui',
-    //   script: path.join(__dirname, 'node_modules/moneyd-gui'),
-    //   node_args: [],
-    //   args: ['--colors'],
-    //   env: {
-    //     PORT: '7770',
-    //     ADMIN_API_PORT: '7769',
-    //     ...debugEnv
-    //   }
-    // }
   ]
 }
-
-let ports = [...Array(os.cpus().length * ecosystem.apps.length)].map((v, i) => {
-  return (
-    (process.env.INSPECT ? parseInt(process.env.INSPECT) : process.debugPort) +
-    i
-  )
-})
-
-ecosystem.apps.forEach(app => {
-  if (process.env.NODE_ENV !== 'production') {
-    let port = execa.shellSync(`get-port ${ports.join(' ')}`).stdout
-    ports = ports.filter(v => v != port)
-    app.node_args.push(`--inspect-brk=${port}`)
-  }
-})
 
 module.exports = ecosystem
