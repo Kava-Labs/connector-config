91b1f0289137440752beab870842fba4c01e9488
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-02-28 15:31:09 -0500

    fix: inspect node process for dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,6 @@ const ecosystem = {
       name: 'ilp-connector',
       script: path.join(__dirname, 'run-connector.js'),
       node_args: [],
-      args: ['--colors'],
       env: {
         ...process.env,
         ...debugEnv
@@ -23,6 +22,10 @@ const ecosystem = {
       log: '~/.pm2/logs/ilp-connector-combined.log'
     }
   ]
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  ecosystem.apps[0].node_args.push('--inspect-brk')
 }
 
 module.exports = ecosystem
