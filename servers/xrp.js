53b3f8fd92e9c5d76ea45937b313c0de525c6213
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-03-11 17:46:15 -0400

    feat: update to new xrp plugin
    - much more reliable
    - breaks existing accounts
    - remove old, unnecessary deps

--- servers/xrp.js
+++ servers/xrp.js
@@ -1,31 +1,25 @@
-const { convert, usd, xrpBase, drop } = require('@kava-labs/crypto-rate-utils')
+const { convert, usd, drop } = require('@kava-labs/crypto-rate-utils')
 
 module.exports = rateBackend => {
-  const outgoingChannelAmount = convert(usd(10), drop(), rateBackend).toNumber()
-  const minIncomingChannelAmount = convert(
-    usd(0.5),
-    drop(),
-    rateBackend
-  ).toNumber()
-  const maxPacketAmount = convert(usd(0.2), xrpBase(), rateBackend)
+  const outgoingChannelAmount = convert(usd(10), drop(), rateBackend)
+  const minIncomingChannelAmount = convert(usd(0.5), drop(), rateBackend)
+  const maxPacketAmount = convert(usd(0.2), drop(), rateBackend)
 
   return {
     relation: 'child',
-    plugin: 'ilp-plugin-xrp-asym-server',
+    plugin: '@kava-labs/ilp-plugin-xrp-paychan',
     assetCode: 'XRP',
-    assetScale: 9,
+    assetScale: 6,
     options: {
-      assetScale: 9,
+      role: 'server',
       port: 7443,
+      xrpSecret: process.env.XRP_SECRET,
       xrpServer: process.env.XRP_SERVER,
-      address: process.env.XRP_ADDRESS,
-      secret: process.env.XRP_SECRET,
-      // Max credit defaults to 0, so it's unnecesssary
-      // Use plugin maxPacketAmount (and not connector middleware) so F08s occur *before* T04s
-      maxPacketAmount,
       // Very asymmetric... you fund a channel for $0.50 in XRP, we'll open one to you for $10!
       outgoingChannelAmount,
-      minIncomingChannelAmount
+      minIncomingChannelAmount,
+      // Use plugin maxPacketAmount (and not connector middleware) so F08s occur *before* T04s
+      maxPacketAmount
     }
   }
 }
